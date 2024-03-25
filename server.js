20268713299a364b18a00576fb1c4e0644bc6d00
TommyZhouuu <XZHOU145@MYSENECA.CA>
2024-03-25 00:07:48 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 /********************************************************************************
-*  WEB322 – Assignment 03
+*  WEB322 – Assignment 04
 * 
 *  I declare that this assignment is my own work in accordance with Seneca's
 *  Academic Integrity Policy:
 * 
 *  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
 * 
-*  Name: Xuesong Zhou Student ID: 127245223 Date: 2024/03/01
+*  Name: Xuesong Zhou Student ID:127245223 Date: 3.24.2024
 *
-*  Published URL:https://github.com/TommyZhouuu/helloworld/blob/main/server.js
+*  Published URL: https://github.com/TommyZhouuu/helloworld/blob/main/server.js
 *
 ********************************************************************************/
+const suvModels = require('./modules/suvModels');
 const express = require('express');
 const path = require('path');
-const suvModels = require('./modules/suvModels'); 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
 
 suvModels.initialize().then(() => {
   app.listen(PORT, () => {
@@ -31,36 +33,33 @@ suvModels.initialize().then(() => {
   process.exit(1);
 });
 
-// Home route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/home.html'));
+  res.render('home'); 
 });
 
-// About route
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/about.html'));
+  res.render('about'); 
 });
 
 app.get('/suv/models', async (req, res) => {
   try {
     const type = req.query.model;
     const models = type ? await suvModels.getModelsByType(type) : await suvModels.getAllModels();
-    res.json(models);
+    res.render('models', { models: models }); 
   } catch (err) {
-    res.status(404).send(err);
+    res.status(404).render('404', { message: "The requested SUV model cannot be found." });
   }
 });
-
 
 app.get('/suv/models/:modelId', async (req, res) => {
   try {
     const model = await suvModels.getModelById(req.params.modelId);
-    res.json(model);
+    res.render('model', { model: model }); 
   } catch (err) {
-    res.status(404).send(err);
+    res.status(404).render('404', { message: "The requested SUV model details could not be found." });
   }
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+  res.status(404).render('404', { message: "Sorry, we can't find that page!" }); 
 });
